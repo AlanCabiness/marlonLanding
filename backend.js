50795889d1c72b52cb84a503ea10629d9bf865aa
AlanCabiness <acab1996@gmail.com>
2018-03-02 16:22:55 -0600

    completed login and new user. Has basic data validation and authentication. Now working on pages for logged in users.

---
+++ backend.js
@@ -0,0 +1,37 @@
+/*document.getElementById('newUser').phone.addEventListener('input', function (e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});*/
+function createUser(){
+    var data = new FormData();
+    data.append('name', document.getElementById('newUser').name.value);
+    data.append('PhoneNumber', document.getElementById('newUser').phone.value);
+    data.append('Email', document.getElementById('newUser').email.value);
+    data.append('Password', document.getElementById('newUser').password.value);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", "http://localhost:51718/api/student/post", false );
+    xmlHttp.send(data);
+    alert(xmlHttp.responseText)
+}
+
+function login(){
+    var email = document.getElementById('loginUser').email.value;
+    var password = document.getElementById('loginUser').password.value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://localhost:51718/api/student/"+email, false );
+    xmlHttp.send(null);
+
+    //var response = xmlHttp.responseText;
+    var data =  JSON.parse(xmlHttp.responseText);
+    //console.log(data.name);
+    //alert(data.name);
+    if (password == data.password){
+        localStorage.session = "loggedin"
+        localStorage.loggedUser = data.name;
+    }
+    else{
+        alert("incorrect username or password, please try again");
+    }
+
+
+}
