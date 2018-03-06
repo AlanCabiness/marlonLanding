7925b38d32e20453de37c8da27262cca2679005f
AlanCabiness <acab1996@gmail.com>
2018-03-05 19:21:21 -0600

    parses file and ready to send to api

--- profilebackend.js
+++ profilebackend.js
@@ -1,6 +1,11 @@
-function sendResume(){
+function sendResume(filename){
+    var file = document.getElementById('fileUpload').uploadedFile.files[0];
+    var reader  = new FileReader();
+    reader.readAsText(file);
+    var text = reader.result;
+    text = text.replace(/\r\n/g, " ");
+    text = text.split(" ");
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", "http://localhost:51718/api/fileup", false );
-    xmlHttp.send(document.getElementById("fileUpload").uploadedFile.value);
-    alert(xmlHttp.responseText);
+    xmlHttp.open( "POST", "http://localhost:51718/api/parsed", false );
+    xmlHttp.send(text);
 }
