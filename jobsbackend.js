6f2da5dbf92f28ea54a8002ffdc7312ec9a377f6
AlanCabiness <acab1996@gmail.com>
2018-03-19 12:19:34 -0500

    email validation, begin table sorting

--- jobsbackend.js
+++ jobsbackend.js
@@ -97,6 +97,8 @@ function jobsModal(jobName) {
         $('#buttonRow').prop("id", ("buttonRow"+i));
     }
 
+    sortTable()
+
 
 
 }
@@ -115,5 +117,41 @@ function companyFilterKey(){
             tr[i].style.display = "none";
         }
 
+    }
+}
+
+
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[0];
+            y = rows[i + 1].getElementsByTagName("TD")[0];
+            // Check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // I so, mark as a switch and break the loop:
+                shouldSwitch= true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
     }
 }
